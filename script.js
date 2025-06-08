f0ef3f265dc0c3b50879a96bb388fdc433899bb7
Olivier2201 <dusabamahoroolivier5@gmail.com>
2025-06-08 09:55:17 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", event => {
+        event.preventDefault();
+        document.querySelector(link.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
+    });
+});
